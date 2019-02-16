6d437dcbde0a4a88e84374574cb26520133ce481
ss-0904 <784183061@qq.com>
2019-02-16 09:27:34 +0800

    渲染home

--- src/components/login.vue
+++ src/components/login.vue
@@ -36,7 +36,9 @@ export default {
             }
           } = res
           if (status === 200) {
-
+            this.$router.push({
+              name: 'home'
+            })
           } else {
             this.$message.error(msg)
           }
