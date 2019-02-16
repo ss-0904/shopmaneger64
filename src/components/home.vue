cddbcf31d9045dc44e015510a5b3b178cbdb25ae
ss-0904 <784183061@qq.com>
2019-02-16 10:28:11 +0800

    引入布局容器

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,15 +1,26 @@
 <template>
-  <div>
-    home---
-  </div>
+  <el-container class="container">
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside class="aside" width="200px">Aside</el-aside>
+      <el-main class="main" >Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
+  .container {
+      height: 100%;
+      background-color: pink;
+  }
+  .aside {
+      background-color: blue;
+  }
+  .main {
+      background-color: yellow;
+  }
 </style>
