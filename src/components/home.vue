952faf7cde45c83be2a249489314af25fcbb2b39
ss-0904 <784183061@qq.com>
2019-02-16 10:44:00 +0800

    头部调整

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,9 +1,21 @@
 <template>
   <el-container class="container">
-    <el-header>Header</el-header>
+    <el-header>
+      <el-row>
+        <el-col :span="4">
+          <img src="@/assets/logo.png" alt="图片加载失败">
+        </el-col>
+        <el-col :span="19" class="middle">
+          <h2>电商后台管理系统</h2>
+        </el-col>
+        <el-col :span="1">
+          <a href="#" class="logout">退出</a>
+        </el-col>
+      </el-row>
+    </el-header>
     <el-container>
       <el-aside class="aside" width="200px">Aside</el-aside>
-      <el-main class="main" >Main</el-main>
+      <el-main class="main">Main</el-main>
     </el-container>
   </el-container>
 </template>
@@ -13,14 +25,23 @@ export default {};
 </script>
 
 <style>
-  .container {
-      height: 100%;
-      background-color: pink;
-  }
-  .aside {
-      background-color: blue;
-  }
-  .main {
-      background-color: yellow;
-  }
+.container {
+  height: 100%;
+  background-color: #b3c0d1;
+}
+.aside {
+  background-color: blue;
+}
+.main {
+  background-color: yellow;
+}
+.middle {
+  line-height: 60px;
+  text-align: center;
+  color: #ffffff;
+}
+.logout {
+  line-height: 60px;
+  text-decoration: none;
+}
 </style>
