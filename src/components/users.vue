c3cc13de576577701d7805e0ed924ff534f5f9eb
ss-0904 <784183061@qq.com>
2019-02-16 21:34:39 +0800

    slot-scope-日期格式

--- src/components/users.vue
+++ src/components/users.vue
@@ -19,11 +19,24 @@
     </el-row>
     <!-- 表格 -->
     <el-table :data="list" style="width: 100%">
-      <el-table-column prop="name" label="#" width="80"></el-table-column>
-      <el-table-column prop="name" label="姓名" width="120"></el-table-column>
-      <el-table-column prop="name" label="邮箱" width="140"></el-table-column>
-      <el-table-column prop="name" label="电话" width="140"></el-table-column>
-      <el-table-column prop="name" label="创建日期" width="140"></el-table-column>
+      <!--
+        id: (...)
+        username: (...)
+        email: (...)
+        mobile: (...)
+        create_time: (...)
+        mg_state: (...)
+        role_name: (...)
+       -->
+      <el-table-column prop="id" label="#" width="80"></el-table-column>
+      <el-table-column prop="username" label="姓名" width="120"></el-table-column>
+      <el-table-column prop="email" label="邮箱" width="140"></el-table-column>
+      <el-table-column prop="mobile" label="电话" width="140"></el-table-column>
+      <el-table-column label="创建日期" width="140">
+        <template slot-scope="list">
+          {{ list.row.create_time | fmtdate }}
+        </template>
+      </el-table-column>
       <el-table-column prop="name" label="用户状态" width="140"></el-table-column>
       <el-table-column prop="name" label="操作" width="200"></el-table-column>
     </el-table>
@@ -37,7 +50,7 @@ export default {
     return {
       query: '',
       pagenum: 1,
-      pagesize: 2,
+      pagesize: 10,
       // 表格数据
       list: []
     }
@@ -50,7 +63,13 @@ export default {
       const AUTH_TOKEN = localStorage.getItem('token')
       this.$http.defaults.headers.common['Authorization'] = AUTH_TOKEN
       const res = await this.$http.get(`users?query=${this.query}&pagenum=${this.pagenum}&pagesize=${this.pagesize}`)
-      console.log(res)
+      // console.log(res)
+      const {data, meta: {msg, status}} = res.data
+      console.log(msg)
+      if (status === 200) {
+        this.list = data.users
+        console.log(this.list)
+      }
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -6,12 +6,18 @@ import 'element-ui/lib/theme-chalk/index.css'
 import '@/assets/css/base.css'
 import App from './App'
 import router from './router'
+import moment from 'moment'
 import axios from 'axios'
 axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
 Vue.prototype.$http = axios
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
+
+// 全局过滤器 - 处理日期格式
+Vue.filter('fmtdate', (v) => {
+  return moment(v).format('YYYY-MM-DD')
+})
 
 /* eslint-disable no-new */
 new Vue({
